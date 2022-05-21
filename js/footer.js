33b50d39f5029bc56cf94bcb414cea1a6347878e
Oliver-SJHuang <105102532+Oliver-SJHuang@users.noreply.github.com>
2022-05-21 12:44:52 +0800

    add nav and footer

---
+++ js/footer.js
@@ -0,0 +1,38 @@
+const createFooter = () =>{
+    let footer = document.querySelector('footer');
+
+    footer.innerHTML = `
+        <div class="footer-content">
+            <img src="src/light-logo.png" class="logo" alt="">
+            <div class="footer-ul-container">
+                <ul class="category">
+                    <li class="category-title">關於</li>
+                    <li><a href="#" class="footer-link">關於我們</a></li>
+                </ul>
+                <ul class="category">
+                    <li class="category-title">二手好物</li>
+                    <li><a href="#" class="footer-link">拍賣區</a></li>
+                </ul>
+                <ul class="category">
+                    <li class="category-title">心情分享</li>
+                    <li><a href="#" class="footer-link">廢文區</a></li>
+                </ul>
+                <ul class="category">
+                    <li class="category-title">找工作、找實習</li>
+                    <li><a href="#" class="footer-link">資源分享</a></li>
+                </ul>
+            </div>
+        </div>
+        <p class="footer-title">公司簡介</p>
+        <p class="info">佛心公司╰(⊙д⊙)╮佛心公司╭(⊙д⊙)╯</p>
+        <div class="footer-social-container">
+            <div>
+                <a href="#" class="social-link">instagram</a>
+                <a href="#" class="social-link">facebook</a>
+            </div>
+        </div>
+        <p class="footer-credit">清交優質交流網</p>
+    `;
+}
+
+createFooter();
---
+++ js/nav.js
@@ -0,0 +1,58 @@
+const createNav = () => {
+    let nav = document.querySelector('.navbar');
+
+    nav.innerHTML = `
+        <div class="nav">
+            <img src="src/light-logo.png" class="brand-logo" alt=""> <!-- logo -->
+            <div class="nav-items">
+                <div class="search"> <!-- search box -->
+                    <input type="text" class="search-box">
+                    <button class="search-btn">搜尋</button>
+                </div>
+                <a>
+                    <img src="src/user.png" id="user-img" alt="">
+                    <div class="login-logout-popup hide">
+                        <p class="account-info">Log in as, name</p>
+                        <button class="btn" id="user-btn">Log out</button>
+                    </div>
+                </a>
+                <a href="#"><img src="src/cart.png" alt=""></a>
+            </div>
+        </div>
+        <ul class="links-container">
+            <li class="link-item"><a href="#" class="link">關於我們</a></li>
+            <li class="link-item"><a href="#" class="link">拍賣區</a></li>
+            <li class="link-item"><a href="#" class="link">廢文區</a></li>
+            <li class="link-item"><a href="#" class="link">資源分享</a></li>
+        </ul>
+    `;
+}
+
+createNav();
+
+const userImageButton = document.querySelector('#user-img');
+const userPopup = document.querySelector('.login-logout-popup');
+const popuptext = document.querySelector('.account-info');
+const actionBtn = document.querySelector('#user-btn');
+
+userImageButton.addEventListener('click', () => {
+    userPopup.classList.toggle('hide');
+})
+
+window.onload = () => {
+    let user = JSON.parse(sessionStorage.user || null);
+    if(user != null){ //user in login status
+        popuptext.innerHTML = `log in as, ${user.name}`;
+        actionBtn.innerHTML = 'log out';
+        actionBtn.addEventListener('click', () => {
+            sessionStorage.clear();
+            location.reload();
+        })
+    }else{  //user not login in
+        popuptext.innerHTML = 'login to order';
+        actionBtn.innerHTML = 'login';
+        actionBtn.addEventListener('click', () => {
+            location.href = '/login';
+        })
+    }
+}

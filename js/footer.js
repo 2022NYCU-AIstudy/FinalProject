e1d9491fcbaab463fbff966e47392fa1bf7c55f9
Oliver-SJHuang <105102532+Oliver-SJHuang@users.noreply.github.com>
2022-06-16 02:18:26 +0800

    edit

--- js/footer.js
+++ js/footer.js
@@ -4,12 +4,6 @@ const createFooter = () =>{
     footer.innerHTML = `
         <p class="footer-title">公司簡介</p>
         <p class="info">佛心公司╰(⊙д⊙)╮佛心公司╭(⊙д⊙)╯</p>
-        <div class="footer-social-container">
-            <div>
-                <a href="#" class="social-link">instagram</a>
-                <a href="#" class="social-link">facebook</a>
-            </div>
-        </div>
         <p class="footer-credit">清交優質交流網</p>
     `;
 }
